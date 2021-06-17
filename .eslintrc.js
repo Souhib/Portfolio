f50ccdae34ccf2fdd49726deeb5c5bb7a150adbc
Souhib <souhib.trabelsi@epitech.eu>
2021-06-18 01:58:55 +0200

    [Bootstrap] Setup the bootstrap of the project (husky eslint prettier)

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    },
+  },
+  rules: {
+    // Warning
+    'no-unused-vars': 1,
+    'no-console': 1,
+    'no-unreachable': 1,
+
+    // Errors
+    'import/no-unresolved': [2, { caseSensitive: false }],
+    'prettier/prettier': 2,
+    'no-duplicate-case': 2,
+    'no-undef': 2,
+
+    // Tolerance
+    'arrow-body-style': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  semi: false,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+}

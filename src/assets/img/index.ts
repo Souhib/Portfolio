27311735b21efc33d866dfc7a3a05dd32d8bffb9
Rakedd <Rayan.keddache@epitech.eu>
2021-06-24 01:31:26 +0200

    [Assets] New images

---
+++ src/assets/img/index.ts
@@ -0,0 +1,13 @@
+import Contact from './contact.svg'
+import Enedis from './enedis.png'
+import Github from './github.svg'
+import Linkedin from './linkedin.svg'
+import ProfilePicture from './profile_picture.png'
+
+export { 
+  Enedis,
+  Github,
+  Linkedin,
+  ProfilePicture,
+  Contact,
+}

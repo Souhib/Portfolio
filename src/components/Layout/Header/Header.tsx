c31e3866a99a6807c24c3a4ee56f3e155625599e
Rakedd <Rayan.keddache@epitech.eu>
2021-06-24 01:32:29 +0200

    [Header] With social icons and all aligned

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -3,26 +3,73 @@ import React from 'react'
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-import picture from 'assets/img/profile_picture.png'
+import { 
+  ProfilePicture, 
+  Linkedin, 
+  Github,
+  Contact
+} from 'assets/img'
 import Stack from 'components/Containers/Stack/Stack'
 import Avatar from 'components/Images/Avatar/Avatar'
 import Subtitle from 'components/Text/Subtitle/Subtitle'
 import Title from 'components/Text/Title/Title'
 
 const Header: React.FunctionComponent = () => {
-
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-
+  const responsiveHorizontal = isMobile ? 'center' : 'left'
   return (
-    <Stack isRow={!isMobile} spacing={2}>
-      <Avatar isBordered={false} variant="big" source={picture} />
-      <Stack spacing={2} horizontalAlign={isMobile ? 'center' : 'left'}>
-        <Title variant="big" textAlign={isMobile ? 'center' : 'left'}>Souhib Keddache</Title>
-        <Subtitle variant="big" textAlign={isMobile ? 'center' : 'left'}>Front End Engineer @ Café</Subtitle>
-        <Subtitle variant="big" textAlign={isMobile ? 'center' : 'left'}>
+    <Stack
+      isRow
+      horizontalAlign='space-between'
+    >
+      <Stack
+        isRow={!isMobile}
+        verticalAlign='center'
+        spacing={2}
+      >
+        <Avatar
+          isBordered={false}
+          variant='huge'
+          source={ProfilePicture}
+        />
+        <Stack spacing={2}>
+          <Title
+            variant='big'
+            textAlign={responsiveHorizontal}
+          >Souhib Keddache</Title>
+          <Subtitle
+            variant='big'
+            textAlign={responsiveHorizontal}
+          >Front End Engineer @ Café</Subtitle>
+          <Subtitle
+            variant='big'
+            textAlign={responsiveHorizontal}
+          >
           #OpenToWork - End of studies internship
-        </Subtitle>
+          </Subtitle>
+        </Stack>
+      </Stack>
+      <Stack
+        isRow
+        verticalAlign='center'
+        spacing={2}
+      >
+        <Avatar
+          isBordered
+          source={Linkedin}
+          variant='medium'
+        />
+        <Avatar
+          isBordered
+          source={Contact}
+          variant='medium'
+        />
+        <Avatar
+          isBordered
+          source={Github}
+          variant='medium'
+        />
       </Stack>
     </Stack>
   )

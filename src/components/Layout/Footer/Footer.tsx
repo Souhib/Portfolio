7cb1a13f8cecab9504c68e3f2c61bbc022f0d53d
Rakedd <Rayan.keddache@epitech.eu>
2021-06-24 01:33:32 +0200

    [Footer] Clean footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,29 +1,45 @@
 import React from 'react'
 
-// import { Box } from '@material-ui/core'
-
+import { Box } from '@material-ui/core'
 
 import github from 'assets/img/github.svg'
 import linkedin from 'assets/img/linkedin.svg'
-import Inline from 'components/Containers/Inline/Inline'
 import Stack from 'components/Containers/Stack/Stack'
 import Avatar from 'components/Images/Avatar/Avatar'
 import Subtitle from 'components/Text/Subtitle/Subtitle'
 
-import useStyles from './styles'
-
 const Footer: React.FunctionComponent = () => {
-  const classes = useStyles()
-
   return (
-    <Stack spacing={2}>
-      <Inline spacing={3}>
-        <Avatar isBordered={true} variant="medium" source={github} />
-        <Avatar isBordered={true} variant="medium" source={linkedin} />
-      </Inline>
-      <Subtitle variant="small">Made by Keddache Rayan</Subtitle>
-      <Subtitle variant="small">Contact@KeddacheRayan.com</Subtitle>
-    </Stack>
+    <Box mt='auto'>
+      <Stack
+        horizontalAlign='center'
+        spacing={2}
+      >
+        <Stack
+          isRow
+          horizontalAlign='center'
+        >
+          <Avatar
+            isBordered={true}
+            variant='medium'
+            source={github}
+          />
+          <Avatar
+            isBordered={true}
+            variant='medium'
+            source={linkedin}
+          />
+        </Stack>
+        <Subtitle
+          textAlign='center'
+          variant='small'
+        >Made by Keddache Rayan</Subtitle>
+        <Subtitle
+          textAlign='center'
+          variant='small'
+        >Contact@KeddacheRayan.com</Subtitle>
+      </Stack>
+    </Box>
   )
 }
 

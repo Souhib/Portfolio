e28819b6f5557a49a61aaf8279473431f878fb9c
pharaoon <caplette29200@gmail.com>
2021-06-23 03:17:59 +0200

    [Stack] Interface

--- src/components/Containers/interfaces.ts
+++ src/components/Containers/interfaces.ts
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
  type ContainerProps = {
   children: ReactNode
   horizontalAlign?: 'left' | 'center' | 'right'
+  isRow?: boolean
   spacing?: number
   verticalAlign?: 'left' | 'center' | 'right'
 }

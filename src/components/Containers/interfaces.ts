df47f252c9e651bdecb10fa83a502417c06c6bc4
Rakedd <Rayan.keddache@epitech.eu>
2021-06-24 01:35:22 +0200

    [Stack interface] New type

--- src/components/Containers/interfaces.ts
+++ src/components/Containers/interfaces.ts
@@ -2,10 +2,10 @@ import { ReactNode } from 'react';
 
  type ContainerProps = {
   children: ReactNode
-  horizontalAlign?: 'left' | 'center' | 'right'
+  horizontalAlign?: 'left' | 'center' | 'right' | 'space-between'
   isRow?: boolean
   spacing?: number
-  verticalAlign?: 'left' | 'center' | 'right'
+  verticalAlign?: 'top' | 'center' | 'bottom'
 }
 
 export default ContainerProps

030d1a41620c7e1ab5b7395288af6f0dac7b11ac
Trabelsi Souhib <souhib.trabelsi@epitech.eu>
2021-07-09 02:08:02 +0200

    [ESLINT] REMOVE unused variable in main

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react'
 
-import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 import { Box, useMediaQuery } from '@material-ui/core'
 import { createStyles, Theme, useTheme } from '@material-ui/core/styles'
 import { makeStyles } from '@material-ui/core/styles'
+import Slider from 'react-slick'
 
 import {
   ChevronLeft,

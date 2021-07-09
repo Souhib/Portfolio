05abdc51b312a5d147e15e21342e902d63ec1b92
Trabelsi Souhib <souhib.trabelsi@epitech.eu>
2021-07-09 02:05:29 +0200

    [ASSETS] Update assets

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,3 +1,4 @@
+import Abbana from './abbana.png'
 import Ahlia from './ahlia.png'
 import AirFrance from './airfrance.png'
 import ChevronLeft from './chevronLeft.svg'
@@ -6,14 +7,21 @@ import Cloud from './cloud.png'
 import Cloudeasier from './cloudeasier.png'
 import Contact from './contact.svg'
 import Cross from './cross.svg'
+import Database from './database.png'
+import DevOps from './devops.png'
 import Enedis from './enedis.png'
 import Epitech from './epitech.png'
 import Github from './github.svg'
+import GRDF from './grdf.png'
+import IIM from './iim.png'
 import Integration from './integration.png'
+import IPAG from './ipag4.png'
 import Kookai from './kookai.png'
 import Levi from './levi.png'
+import Lilibow from './lilibow.png'
 import Linkedin from './linkedin.svg'
 import Mangas from './mangas.png'
+import Praetorium from './praetorium.png'
 import ProfilePicture from './profile_picture.png'
 import Python from './python.svg'
 import React from './react.svg'
@@ -23,6 +31,7 @@ import Typescript from './typescript.svg'
 import VueJS from './vuejs.svg'
 
 export {
+  Abbana,
   Ahlia,
   AirFrance,
   ChevronLeft,
@@ -31,15 +40,22 @@ export {
   Cloudeasier,
   Contact,
   Cross,
+  Database,
+  DevOps,
   Enedis,
   Epitech,
   Github,
+  GRDF,
+  IIM,
   Integration,
+  IPAG,
   Kookai,
   Levi,
+  Lilibow,
   Linkedin,
   Mangas,
   ProfilePicture,
+  Praetorium,
   Python,
   React,
   Ruby,

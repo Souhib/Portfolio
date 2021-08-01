a7decf426c86accd5feef1fc6888fcf403954d5b
Trabelsi Souhib <souhib.trabelsi@epitech.eu>
2021-08-01 04:00:37 +0200

    [Analytics] ADD Google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import ReactGa from 'react-ga'
 
 import './App.css'
 import Main from './Main'
 
+ReactGa.initialize('G-G6L04L5260')
 const App: React.FunctionComponent = () => {
+
+  useEffect(() => {
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
+  
   return <Main />
 }
 
